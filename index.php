79493e832fc18a3c7eaff9cf424dfe35342a57cb
iam <163867567+Ian123oi@users.noreply.github.com>
2025-06-13 00:09:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Criptonumeros...</title>
-    <style>
-        body {
-        background-color: lightblue;
-        }
-    </style>
+   
 </head>
 <body>
     
             <h1> cifra informático </h1>
+
+    <br> <br>
+    <h1> A Criptografia funciona coletando a mensagem e transformando-a em número, pegando seu código ASCII e colocando em um vetor. Com isso, ela altera o número pegan o valor do primeiro elemento e somando com o seguinte, depois
+        do segundo elemento e soma com o seguinte, assim sucessivamente até chegar no último, onde soma com o primeiro elemento já modificado, e então, retorna o valor criptografado em forma de números separados por pontos.
 
     <form action="mensagem.php" method="POST">
 
